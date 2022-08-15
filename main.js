fb3cfc7a34eae7b4d5fe080d5b90cd68f751414d
Elliot Mangini <elliot.mangini@gmail.com>
2022-08-15 10:39:38 -0600

    commit tree b410e10f06f69cd5cbf443eac738d06a46fd58b0

--- main.js
+++ main.js
@@ -4,8 +4,39 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+// toggles glyph
+const glyphHandler = (e) => {
+  if (e.target.textContent === EMPTY_HEART) {
+    e.target.classList.add('activated-heart');
+    e.target.textContent = FULL_HEART;
+  } else {
+    e.target.textContent = EMPTY_HEART;
+    e.target.classList.remove('activated-heart');
+  }
+}
+
+let glyphs = document.querySelectorAll('.like-glyph')
+
+glyphs.forEach(glyph => {
+
+  glyph.addEventListener('click', function changeGlyph (e) {
+
+    mimicServerCall(e.target)
+    //optimistically changing the DOM.
+    .then(glyphHandler(e))
+    .catch((error) => {
+      //correcting an optimistic change.
+      glyphHandler(e)
+      let modal = document.querySelector('.hidden');
+      modal.textContent = error;
+      modal.removeAttribute('class');
+      setTimeout(function () {modal.classList.add('hidden')}, 3000);
+    })
 
 
+  })
+
+});
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
